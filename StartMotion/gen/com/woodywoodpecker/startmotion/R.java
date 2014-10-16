1e71a976761771827d3d2e333c783c1b5cc669e9
Umahizi <iskra_i_popova@mail.bg>
2014-10-16 22:12:19 +0300

    prety

--- StartMotion/gen/com/woodywoodpecker/startmotion/R.java
+++ StartMotion/gen/com/woodywoodpecker/startmotion/R.java
@@ -18,13 +18,14 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int text_size=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int text_size=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int stub=0x7f020001;
+        public static final int button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int stub=0x7f020002;
     }
     public static final class id {
         public static final int TextView01=0x7f080014;
@@ -71,36 +72,36 @@ public final class R {
         public static final int set_project=0x7f070004;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int album_name=0x7f05001a;
-        public static final int all_images=0x7f05001b;
-        public static final int app_name=0x7f050000;
-        public static final int btnIntend=0x7f050016;
-        public static final int cannot=0x7f050019;
-        public static final int downloaded_images=0x7f05001c;
-        public static final int hello_world=0x7f050001;
-        public static final int instruct=0x7f050018;
-        public static final int invalid_iser=0x7f05001d;
-        public static final int list_images_actovity=0x7f050009;
-        public static final int login=0x7f050012;
-        public static final int photo_activity=0x7f050008;
-        public static final int picture=0x7f050015;
-        public static final int refresh=0x7f050017;
-        public static final int register=0x7f050013;
-        public static final int remember_me=0x7f050014;
-        public static final int title_activity_create=0x7f050005;
-        public static final int title_activity_register=0x7f050003;
-        public static final int title_activity_save_and_share=0x7f050006;
-        public static final int title_activity_set_project=0x7f050004;
-        public static final int user_email=0x7f05000e;
-        public static final int user_info=0x7f050007;
-        public static final int user_name=0x7f05000b;
-        public static final int user_name_hint=0x7f050010;
-        public static final int user_pass=0x7f05000c;
-        public static final int user_pass_conf=0x7f05000d;
-        public static final int user_pass_data=0x7f050011;
-        public static final int user_pass_hint=0x7f05000f;
-        public static final int welcome=0x7f05000a;
+        public static final int action_settings=0x7f060002;
+        public static final int album_name=0x7f06001a;
+        public static final int all_images=0x7f06001b;
+        public static final int app_name=0x7f060000;
+        public static final int btnIntend=0x7f060016;
+        public static final int cannot=0x7f060019;
+        public static final int downloaded_images=0x7f06001c;
+        public static final int hello_world=0x7f060001;
+        public static final int instruct=0x7f060018;
+        public static final int invalid_iser=0x7f06001d;
+        public static final int list_images_actovity=0x7f060009;
+        public static final int login=0x7f060012;
+        public static final int photo_activity=0x7f060008;
+        public static final int picture=0x7f060015;
+        public static final int refresh=0x7f060017;
+        public static final int register=0x7f060013;
+        public static final int remember_me=0x7f060014;
+        public static final int title_activity_create=0x7f060005;
+        public static final int title_activity_register=0x7f060003;
+        public static final int title_activity_save_and_share=0x7f060006;
+        public static final int title_activity_set_project=0x7f060004;
+        public static final int user_email=0x7f06000e;
+        public static final int user_info=0x7f060007;
+        public static final int user_name=0x7f06000b;
+        public static final int user_name_hint=0x7f060010;
+        public static final int user_pass=0x7f06000c;
+        public static final int user_pass_conf=0x7f06000d;
+        public static final int user_pass_data=0x7f060011;
+        public static final int user_pass_hint=0x7f06000f;
+        public static final int welcome=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -124,10 +125,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f040001;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f040002;
+        public static final int button=0x7f040000;
     }
 }
