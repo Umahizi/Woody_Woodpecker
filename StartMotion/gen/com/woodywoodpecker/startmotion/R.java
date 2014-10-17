8fb55c35118159d897e331871bb1fb6b6b7134db
delyan-nikolov-1992 <delyan.nikolov.1992@gmail.com>
2014-10-17 12:40:29 +0300

    Added Description

--- StartMotion/gen/com/woodywoodpecker/startmotion/R.java
+++ StartMotion/gen/com/woodywoodpecker/startmotion/R.java
@@ -23,11 +23,9 @@ public final class R {
         public static final int text_size=0x7f050002;
     }
     public static final class drawable {
-        public static final int back=0x7f020000;
-        public static final int button=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int loginback=0x7f020003;
-        public static final int stub=0x7f020004;
+        public static final int button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int stub=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f080012;
